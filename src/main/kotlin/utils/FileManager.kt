36ac61370c9cec82c67c753257f1cc9152ea8df3
Ratnesh <ratneshjain40@gmail.com>
2024-12-10 19:54:07 +0530

    Make data dirs

--- src/main/kotlin/utils/FileManager.kt
+++ src/main/kotlin/utils/FileManager.kt
@@ -3,6 +3,18 @@ package com.github.ratneshjain40.utils
 import java.io.File
 
 class FileManager(private val dataDir: String) {
+
+    init {
+        val testDir = File("$dataDir\\test")
+        if (!testDir.exists()) {
+            testDir.mkdirs()
+        }
+        val inputDir = File("$dataDir\\input")
+        if (!inputDir.exists()) {
+            inputDir.mkdirs()
+        }
+    }
+
     fun readFile(fileName: String): String {
         val file = File(fileName)
         val txt = file.readText()
